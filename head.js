abb0fa42c8e1deb236fdd1cb177043f2aeedbd53
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-05-25 23:27:59 +0000

    Implementing head

--- head.js
+++ head.js
@@ -4,7 +4,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`)
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
@@ -12,9 +12,9 @@ const assertEqual = function(actual, expected) {
 const head = function(array) {
   let headElement = array[0];
   return headElement;
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), );
+assertEqual(head([]),);
 
