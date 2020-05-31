1c1ee8250fe72445907708e1438d3078458cbdbb
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-05-31 16:57:02 +0000

    Lotide Refactor Example

--- assertEqual.js
+++ assertEqual.js
@@ -2,15 +2,14 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    //console.log("✅Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
+    //console.log("🛑Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
+module.exports = assertEqual;
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(10, 1);
-assertEqual("Bootcamp", "Bootcamp");
--- head.js
+++ head.js
@@ -1,20 +1,11 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 
 const head = function(array) {
   let headElement = array[0];
   return headElement;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),);
+module.exports = head;
+
+
 
