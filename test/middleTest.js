f71e50a27c9563fe71502564eb6dfa0048a81a49
reshmaushakumari <reshma.ushakumari@outlook.com>
2020-06-01 04:03:47 +0000

    Introducing index.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,6 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
 
-
-// assertArraysEqual(middle([1,2,3]),[2]);
-// assertArraysEqual((middle([1])),[]); // => []
-// assertArraysEqual(middle([1, 2]),[]); // => []
-// assertArraysEqual(middle([1, 2, 3, 4]),[2,3]); // => [2, 3]
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]); // => [3, 4]
-// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-
 describe("#tail", () => {
   it("To check the array with 3 values", () => {
     assert.deepEqual(middle([1,2,3]),[2]);
